8b1e14e261b90cfe71217c7273b1b9c4aa6ebcbc
dveerendar01 <veerendarrathod4@gmail.com>
2026-06-15 01:21:00 +0530

    Add solution of Leetcode #217 Contains Duplicate

---
+++ 01_Arrays/0127_Contains_Duplicate.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> set;
+
+        for(int num:nums) {
+            if(set.find(num) != set.end()) {
+                return true;
+            }
+            set.insert(num);
+        }
+        return false;
+    }
+};
