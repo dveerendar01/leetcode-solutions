45330901fcdbb84455d673e03614ca6e12a53bcd
dveerendar01 <veerendarrathod4@gmail.com>
2026-06-16 22:56:24 +0530

    Add solution for Leetcode #268 Missing Number

---
+++ 01_Arrays/0268_Missing_Number.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int expected=n*(n+1)/2;
+        int actual=0;
+
+        for(int num:nums) {
+            actual = actual+num;
+        }
+        return expected-actual;
+    }
+};
