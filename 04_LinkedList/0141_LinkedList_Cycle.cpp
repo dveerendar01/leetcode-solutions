4733d812424fc016e8452371d0838b0ff463485d
dveerendar01 <veerendarrathod4@gmail.com>
2026-06-19 17:44:36 +0530

    Add solution for Leetcode #141 Linked List Cycle

---
+++ 04_LinkedList/0141_LinkedList_Cycle.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int value;
+    ListNode* next;
+
+    ListNode(int x) {
+        value=x;
+        next= nullptr;
+    }
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode*slow = head;
+        ListNode*fast = head;
+
+        while(fast != nullptr && fast->next != nullptr) {
+            slow= slow->next;
+            fast= fast->next->next;
+
+            if(slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
