7c06aa69150dd0c36f207ac4ad93436d99c01737
dveerendar01 <veerendarrathod4@gmail.com>
2026-07-25 21:58:59 +0530

    Add solution for Leetcode #709 To Lower Case

---
+++ 02_Strings/0709_To_Lower_Case.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(int i=0; i<s.length(); i++) {
+            if(s[i] >= 'A' && s[i] <= 'Z') {
+                s[i] = s[i] + 32;
+            }
+        }
+        return s;
+    }
+};
