d16282264590b3166a7e9e57dd0b185ff7b752de
dveerendar01 <veerendarrathod4@gmail.com>
2026-07-26 23:57:37 +0530

    Add solution for Leetcode #1207 Unique Number of Occurrences

---
+++ 03_Hashing/1207_Unique_Number_of_Occurrences.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int> &arr) {
+        unordered_map<int, int> freq;
+        for (int num : arr) {
+            freq[num]++;
+        }
+        unordered_set<int> seen;
+        for (auto it : freq) {
+            if (seen.count(it.second)) {
+                return false;
+            }
+            seen.insert(it.second);
+        }
+        return true;
+    }
+};
