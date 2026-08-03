ba01e1562883d21e79ce83d5b9aa947d421b0691
dveerendar01 <veerendarrathod4@gmail.com>
2026-08-03 22:26:17 +0530

    Add solution for Leetcode #657 Robot Return to Origin

---
+++ 02_Strings/0657_Robot_Return_to_Origin.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x=0;
+        int y=0;
+        
+        for(char move : moves) {
+            if(move == 'U') {
+                y++;
+            } else if(move == 'D') {
+                y--;
+            } else if(move == 'L') {
+                x--;
+            } else {
+                x++;
+            }
+        }
+        return (x == 0 && y == 0);
+    }
+};
