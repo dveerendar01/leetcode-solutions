5a689adc12d007f6d24db2e9d3c093d991ec15b8
dveerendar01 <veerendarrathod4@gmail.com>
2026-08-05 18:38:41 +0530

    Add solution for Leetcode #917 Reverse Only Letters

---
+++ 12_TwoPointers/0917_Reverse_Only_Letters.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int left = 0;
+        int right = s.length()-1;
+        while(left < right) {
+            if(!isalpha(s[left])) {
+                left++;
+            } else if(!isalpha(s[right])) {
+                right--;
+            } else {
+                swap(s[left], s[right]);
+                left++;
+                right--;
+            }
+        }
+        return s;
+    }
+};
